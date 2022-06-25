109e5687d81566d4b417cf3ccd5a6a56d2d171e3
Aigul <sharipova.aig@gmail.com>
2022-06-25 18:57:09 -0400

    js

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 const image = document.querySelector('.photo');
 const button = document.querySelector('.getJoke'); 
+button.addEventListener('click', function(){
+  getRandomJoke();
+})
+function getRandomJoke(){
+  const ajax = new XMLHttpRequest;
+  const url = 'https://api.chucknorris.io/jokes/random'
+
+  ajax.open('GET', url, true);
+
+  ajax.onreadystatechange = function(){
+      if(this.status === 200 && this.readyState === 4){
+          console.log(this.responseText);
+          let data = JSON.parse(this.responseText);
+          jokeDIV.innerHTML = `${data.value}`
+      } else {
+          this.onerror = onerror();
+      }
+  }
+  ajax.send();
+}
+
+function onerror(){
+   jokeDIV.textContent = 'There was an error!';  
+}
